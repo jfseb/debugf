bea754cf900892ba977ccc7c1e3e21992742131e
jfseb <jfsebpublic@gmail.com>
2017-05-12 23:53:10 +0200

    add typings

--- js/index.js
+++ js/index.js
@@ -1,15 +1,16 @@
-var debug = require('debug');
+"use strict";
+var debugo = require('debug');
 function debugf(s) {
-    var debugi = debug(s);
-    return new Proxy(debugi, {
-        apply: function (target, thisArg, arguments) {
+    var debugi = debugo(s);
+    var res = new Proxy(debugi, {
+        apply: function (target, thisArg, args) {
             if (debugi.enabled) {
-                var argsevaled = arguments.map(a => (typeof a === 'function') ? a() : a);
-                return debugi.apply(debug, argsevaled);
+                var argsevaled = args.map(a => (typeof a === 'function') ? a() : a);
+                return debugi.apply(debugi, argsevaled);
             }
         }
     });
+    return res;
 }
 module.exports = debugf;
-
-//# sourceMappingURL=index.js.map
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi9zcmMvaW5kZXgudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IjtBQUFBLElBQUksTUFBTSxHQUFJLE9BQU8sQ0FBQyxPQUFPLENBQUMsQ0FBQztBQUUvQixnQkFBZ0IsQ0FBVTtJQU14QixJQUFJLE1BQU0sR0FBRyxNQUFNLENBQUMsQ0FBQyxDQUFDLENBQUM7SUFDdkIsSUFBSSxHQUFHLEdBQUcsSUFBSSxLQUFLLENBQUMsTUFBTSxFQUFFO1FBQzFCLEtBQUssRUFBRyxVQUFTLE1BQU0sRUFBRSxPQUFPLEVBQUUsSUFBSTtZQUNwQyxFQUFFLENBQUEsQ0FBQyxNQUFNLENBQUMsT0FBTyxDQUFDLENBQUMsQ0FBQztnQkFDbEIsSUFBSSxVQUFVLEdBQUcsSUFBSSxDQUFDLEdBQUcsQ0FBQyxDQUFDLElBQUksQ0FBQyxPQUFPLENBQUMsS0FBSyxVQUFVLENBQUMsR0FBRSxDQUFDLEVBQUUsR0FBRyxDQUFDLENBQUUsQ0FBQztnQkFDcEUsTUFBTSxDQUFDLE1BQU0sQ0FBQyxLQUFLLENBQUMsTUFBTSxFQUFDLFVBQVUsQ0FBQyxDQUFDO1lBQ3pDLENBQUM7UUFDSCxDQUFDO0tBQ0YsQ0FBUSxDQUFDO0lBQ1YsTUFBTSxDQUFDLEdBQUcsQ0FBQztBQUNiLENBQUM7QUFFRCxpQkFBUyxNQUFNLENBQUMifQ==
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,53 @@
-var debug  = require('debug');
+var debugo  = require('debug');
 
-function debugf(s) {
-  var debugi = debug(s);
-  return new Proxy(debugi, {
-    apply : function(target, thisArg, arguments) {
+function debugf(s : string) :  {
+        (formatter: any, ...args: any[]): void;
+        enabled: boolean;
+        log: Function;
+        namespace: string;
+    } {
+  var debugi = debugo(s);
+  var res = new Proxy(debugi, {
+    apply : function(target, thisArg, args) {
       if(debugi.enabled) {
-        var argsevaled = arguments.map(a => (typeof a === 'function')? a() : a );
-        return debugi.apply(debug,argsevaled);
+        var argsevaled = args.map(a => (typeof a === 'function')? a() : a );
+        return debugi.apply(debugi,argsevaled);
       }
     }
-  });
+  }) as any;
+  return res;
 }
+//var debugx = debugf;
+export = debugf;
+//export default debugf;
+//module.exports = debugf;
+//declare var debug: debug.IDebug;
+//export = debug;
+//public namespace debug;
 
-module.exports = debugf;
+declare namespace debug {
+    export interface IDebug {
+        (namespace: string): debug.IDebugger,
+        coerce: (val: any) => any,
+        disable: () => void,
+        enable: (namespaces: string) => void,
+        enabled: (namespaces: string) => boolean,
+
+        names: string[],
+        skips: string[],
+
+        formatters: IFormatters
+    }
+
+    export interface IFormatters {
+        [formatter: string]: Function
+    }
+
+    export interface IDebugger {
+        (formatter: any, ...args: any[]): void;
+
+        enabled: boolean;
+        log: Function;
+        namespace: string;
+    }
+}
