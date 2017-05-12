f377a3917a4b52aae409975a5a9f010966a4e37d
jfseb <jfsebpublic@gmail.com>
2017-05-12 22:44:57 +0200

    Test not called

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,9 @@ function debugf(s) {
   var debugi = debug(s);
   return new Proxy(debugi, {
     apply : function(target, thisArg, arguments) {
-      if(debugi.enabled && typeof arguments[0] === 'function') {
-        debugi(arguments[0]());
-      } else {
-        return debugi.apply(debug,arguments);
+      if(debugi.enabled) {
+        var argsevaled = arguments.map(a => (typeof a === 'function')? a() : a );
+        return debugi.apply(debug,argsevaled);
       }
     }
   });
--- test/index_not_called.nunit.js
+++ test/index_not_called.nunit.js
@@ -6,8 +6,8 @@ var process = require('process');
 // (e.g. not via gulp-nodeunit)
 // presumably debug reads the environment variable once and does not react on later calls
 
+// process.env['DEBUG'] = 'test1';
 
-process.env['DEBUG'] = 'test1';
 var root = (process.env.FSD_COVERAGE) ? '../js_cov' : '../js';
 var debugf = require(root + '/index.js');
 
@@ -18,14 +18,11 @@ exports.testParser  = function (test) {
   var logged = [];
   s1.log = function(a) { logged.push(a);};
   s1('here we go');
-  s1(function() { return 'here we come from a function'; });
+  s1(function() { test.equal(1,0); throw new Error('should not get here'); });
   s1('ab %O ', () => { return { a : 1};});
   debugorig('abc');
   test.deepEqual(logged,
-    [ '  \u001b[31;1mtest1 \u001b[0mhere we go',
-      '  \u001b[31;1mtest1 \u001b[0mhere we come from a function',
-      '  \u001b[31;1mtest1 \u001b[0mab { a: \u001b[33m1\u001b[39m } '
-    ]);
+    [ ]);
   test.done();
 };
 
